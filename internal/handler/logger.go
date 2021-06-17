5c754e2174dee9bf1408a44a0577cb9100c00ff9
caijunduo <1176048943@qq.com>
2021-06-17 10:09:56 +0800

    feat: 修正Logger Middleware

--- internal/handler/logger.go
+++ internal/handler/logger.go
@@ -10,17 +10,25 @@ func Logger(c *gin.Context) {
     start := time.Now()
     path := c.Request.URL.Path
     query := c.Request.URL.RawQuery
+    postForm := c.Request.PostForm
+    body := c.Request.Body
+    headers := c.Request.Header
+
     c.Next()
 
-    cost := time.Since(start)
+    latency := time.Since(start)
+    
     zap.L().Info("[Request]",
         zap.Int("status", c.Writer.Status()),
         zap.String("method", c.Request.Method),
         zap.String("path", path),
         zap.String("query", query),
+        zap.Any("post-form", postForm),
+        zap.Any("body", body),
+        zap.Any("headers", headers),
         zap.String("ip", c.ClientIP()),
         zap.String("user-agent", c.Request.UserAgent()),
         zap.String("errors", c.Errors.ByType(gin.ErrorTypePrivate).String()),
-        zap.Duration("latency", cost),
+        zap.Duration("latency", latency),
     )
 }
