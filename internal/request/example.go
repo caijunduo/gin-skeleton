cd9704cf74cf0bbfdd8daaa0e65bd16499b8e20d
caijunduo <caijunduo@gmail.com>
2021-07-12 14:18:32 +0800

    feat: 修改Request和Response

--- internal/request/example.go
+++ internal/request/example.go
@@ -1,5 +1,5 @@
 package request
 
-type ExampleRequest struct {
-    PaginationRequest
+type Example struct {
+    Pagination
 }
--- internal/request/pagination.go
+++ internal/request/pagination.go
@@ -1,6 +1,11 @@
 package request
 
-type PaginationRequest struct {
+type Pagination struct {
     Page    int64 `json:"page" xml:"page" query:"page" form:"page" protobuf:"page" uri:"page" binding:"gte=1"`
     PerPage int64 `json:"per_page" xml:"per_page" query:"per_page" form:"per_page" protobuf:"per_page" uri:"per_page" binding:"gte=1,lte=50"`
+}
+
+func (r *Pagination) DefaultPagination() {
+    r.Page = 1
+    r.PerPage = 20
 }
